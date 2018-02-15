fe003c1574d2c2ff2b4f7d814a31dba91ffbff34
Austin Browne <aus.bro22@gmail.com>
2018-02-15 15:55:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase 
+ 
+mixedCase.toUpperCase() === mixedCase;
 
 function shout(string) {
   return string.toUpperCase()
