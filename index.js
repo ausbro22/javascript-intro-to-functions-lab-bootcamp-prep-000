963d7e6b7a9e61e9b39b022e2976f2144f51e5fd
Austin Browne <aus.bro22@gmail.com>
2018-02-15 16:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string == "I love you, Grandma.") {
     return("I love you, too.")
-  }else if (string == uppercase) {
+  }else if (string === uppercase) {
     return("YES INDEED!")
-  } else if (string == lowercase) {
+  } else if (string === lowercase) {
     return "I can\'t hear you!"
   }
 }
