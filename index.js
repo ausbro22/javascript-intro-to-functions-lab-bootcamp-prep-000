026357ca221a74d60f12c5d5a3bfe4a9d99f4759
Austin Browne <aus.bro22@gmail.com>
2018-02-15 15:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function whisper(string){
   return string.toLowerCase()
 }
 function sayHiToGrandma(string){
-  if(string = lowercase){
+  if(string == lowercase){
     console.log("I can't hear you!")
+  }else if(string === uppercase){
+    console.log("YES INDEED!")
   }
 }
